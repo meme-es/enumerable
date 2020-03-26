5f2510fa9f5710add73cd56c84f2ccdc7e2f5588
Meme <meme_es@hotmail.com>
2020-03-26 16:22:13 -0600

    Add the methods my_all?, my_any? and my_none?
    This commit delivers the implementation of the methods:
    - my_all?
    - my_any?
    - my_none?

--- enumerable.rb
+++ enumerable.rb
@@ -20,4 +20,69 @@ module Enumerable
     my_each { |item| array << item if yield(item) }
     array
   end
+
+  def my_all?(parameter = nil)
+    return my_all_param(parameter) unless parameter.nil?
+
+    (my_each { |item| return false unless yield(item) }) if block_given?
+
+    my_each { |item| return false if item.nil? || item == false }
+    true
+  end
+
+  def my_all_param(parameter)
+    if parameter.class == Class
+      my_all? { |item| item.class == parameter }
+    elsif parameter.class == Regexp
+      my_all? { |item| item.match(parameter) }
+    else
+      my_all? { |item| item == parameter }
+    end
+  end
+
+  def my_any?(parameter = nil)
+    return my_any_param(parameter) unless parameter.nil?
+
+    (my_each { |item| return true if yield(item) }) if block_given?
+
+    my_each { |item| return true if !item.nil? && item != false }
+    false
+  end
+
+  def my_any_param(parameter)
+    if parameter.class == Class
+      my_any? { |item| item.class == parameter }
+    elsif parameter.class == Regexp
+      my_any? { |item| item.match(parameter) }
+    else
+      my_any? { |item| item == parameter }
+    end
+  end
+
+  def my_none?(parameter = nil)
+    return my_none_param(parameter) unless parameter.nil?
+
+    return no_block_given unless block_given?
+
+    my_each { |item| return false if yield(item) }
+    true
+  end
+
+  def no_block_given
+    return true if (length == 1 && self[0]) || empty?
+
+    my_each { |item| return false if item }
+
+    true
+  end
+
+  def my_none_param(parameter)
+    if parameter.class == Class
+      my_none? { |item| item.class == parameter }
+    elsif parameter.class == Regexp
+      my_none? { |item| item =~ parameter }
+    else
+      my_none? { |item| item == parameter }
+    end
+  end
 end
