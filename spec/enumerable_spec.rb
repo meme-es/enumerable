94ce82efcf80c539f253967363a981bea7780aa9
Meme <meme_es@hotmail.com>
2020-04-09 18:41:35 -0600

    Add specs to test the #my_all? method
    This commit adds specs to test #my_all? method when there's not a block given,
    when there is a block given and when there is an expression as a parameter.

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -5,7 +5,10 @@ require './enumerable'
 RSpec.describe Enumerable do
   let(:numbers) { [10, 20, 30, 40, 3] }
   let(:str_numbers) { %w[5 10 15 20 25] }
-  let(:animals) { %w[tigger fox owl camel] }
+  let(:animals) { %w[eagle camel horse hyena] }
+  let(:birds) { %w[crow peacock turkey pigeon] }
+  let(:num_with_nil) { [1, 2, 3, 4, nil] }
+  let(:same_num) { [5, 5, 5, 5, 5] }
 
   describe '#my_each' do
     context 'When there is no block given' do
@@ -48,9 +51,67 @@ RSpec.describe Enumerable do
       end
     end
 
-    context 'When there is a operation given as a parameter' do
+    context 'When there is a block given as a parameter' do
       it 'returns an array with the matching elements of the operation' do
         expect(numbers.my_select(&:even?)).to eql(numbers.select(&:even?))
+      end
+    end
+  end
+
+  describe '#my_all?' do
+    context 'When there is no block given' do
+      it 'returns true if there is not nil or false items' do
+        expect(animals.my_all?).to eql(animals.all?)
+      end
+
+      it 'returns false if any of the items is nil or false' do
+        expect(arr_with_nil.my_all?).to eql(arr_with_nil.all?)
+      end
+
+      it 'returns true if the array is empty' do
+        expect([].my_all?).to eql([].all?)
+      end
+    end
+
+    context 'When there is a block given' do
+      it 'returns true if all items pass the block test' do
+        expect(birds.my_all? { |item| item.length >= 4 }).to eql(birds.all? { |item| item.length >= 4 })
+      end
+
+      it "returns false if any item doesn't pass the block test" do
+        expect(birds.my_all? { |item| item.length == 6 }).to eql(birds.all? { |item| item.length == 6 })
+      end
+    end
+
+    context 'When there is a parameter given as' do
+      context 'a regular expression' do
+        it 'returns true if all items match the given expression' do
+          expect(animals.my_all?(/e/)).to eql(animals.all?(/e/))
+        end
+
+        it "returns false if any item doesn't match the given expression" do
+          expect(animals.my_all?(/a/)).to eql(animals.all?(/a/))
+        end
+      end
+
+      context 'a literal' do
+        it 'returns true if all elements have the same value' do
+          expect(same_num.my_all?(5)).to eql(same_num.all?(5))
+        end
+
+        it "returns false if any elements haven't the same value" do
+          expect(numbers.my_all?(3)).to eql(num_with_nil.all?(3))
+        end
+      end
+
+      context 'a class' do
+        it 'returns true if all elements are instances of the class given' do
+          expect(numbers.my_all?(Integer)).to eql(numbers.all?(Integer))
+        end
+
+        it "returns false if any element isn't an instance of the class given" do
+          expect(num_with_nil.my_all?(Integer)).to eql(num_with_nil.all?(Integer))
+        end
       end
     end
   end
